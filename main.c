bc9c1beb73ce42546c7cc59cd5445bb53159605b
Roy Salazar <51371955+roysalazarp@users.noreply.github.com>
2024-06-05 20:07:33 +0000

    added http request parser

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <ctype.h>
 #include <errno.h>
 #include <pthread.h>
 #include <signal.h>
@@ -7,17 +8,30 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct ClientSocketQueueNode {
+    struct ClientSocketQueueNode *next;
+    int *client_socket;
+} ClientSocketQueueNode;
+
+typedef struct {
+    char http_version[10];
+    char url[255];
+    char method[10];
+    char *query_params;
+    char *headers;
+    char *body;
+} HttpRequest;
+
 void sigint_handler(int signo);
 int router(void *p_client_socket, uint8_t thread_index);
 void *thread_function(void *arg);
 void enqueue_client_socket(int *client_socket);
 void *dequeue_client_socket();
-int home_get(int client_socket, uint8_t thread_index);
-
-typedef struct ClientSocketQueueNode {
-    struct ClientSocketQueueNode *next;
-    int *client_socket;
-} ClientSocketQueueNode;
+int home_get(int client_socket, HttpRequest *request, uint8_t thread_index);
+int read_request(char **request_buffer, int client_socket);
+int parse_http_request(HttpRequest *parsed_http_request, const char *http_request);
+void http_request_free(HttpRequest *parsed_http_request);
+int not_found(int client_socket, HttpRequest *request);
 
 #define MAX_CONNECTIONS 100
 #define POOL_SIZE 3
@@ -230,17 +244,263 @@ int router(void *p_client_socket, uint8_t thread_index) {
     free(p_client_socket);
     p_client_socket = NULL;
 
-    if (home_get(client_socket, thread_index) == -1) {
-        printf("Failed to process request\n");
+    char *request = NULL;
+    if (read_request(&request, client_socket) == -1) {
         retval = -1;
-        goto cleanup;
+        goto cleanup_request_buffer;
     }
 
-cleanup:
+    if (strlen(request) == 0) {
+        fprintf(stderr, "Request is empty\nError code: %d\n", errno);
+        retval = 0;
+        goto cleanup_request_buffer;
+    }
+
+    HttpRequest parsed_http_request = {0};
+    if (parse_http_request(&parsed_http_request, request) == -1) {
+        retval = -1;
+        goto cleanup_parsed_request;
+    }
+
+    if (strcmp(parsed_http_request.url, "/") == 0) {
+        if (strcmp(parsed_http_request.method, "GET") == 0) {
+            if (home_get(client_socket, &parsed_http_request, thread_index) == -1) {
+                retval = -1;
+                goto cleanup_parsed_request;
+            }
+
+            goto cleanup_parsed_request;
+        }
+    } else {
+        if (not_found(client_socket, &parsed_http_request) == -1) {
+            retval = -1;
+            goto cleanup_parsed_request;
+        }
+
+        goto cleanup_parsed_request;
+    }
+
+cleanup_parsed_request:
+    http_request_free(&parsed_http_request);
+
+cleanup_request_buffer:
+    free(request);
+    request = NULL;
+
     return retval;
 }
 
-int home_get(int client_socket, uint8_t thread_index) {
+int read_request(char **request_buffer, int client_socket) {
+    size_t buffer_size = 1024;
+
+    *request_buffer = (char *)malloc((buffer_size * (sizeof **request_buffer)) + 1);
+    if (*request_buffer == NULL) {
+        fprintf(stderr, "Failed to allocate memory for *request_buffer\nError code: %d\n", errno);
+        return -1;
+    }
+
+    (*request_buffer)[0] = '\0';
+
+    size_t chunk_read = 0;
+    int bytes_read;
+    while ((bytes_read = recv(client_socket, (*request_buffer) + chunk_read, buffer_size - chunk_read, 0)) > 0) {
+        chunk_read += bytes_read;
+
+        if (chunk_read >= buffer_size) {
+            buffer_size *= 2;
+            *request_buffer = realloc((*request_buffer), buffer_size);
+            if (*request_buffer == NULL) {
+                fprintf(stderr, "Failed to reallocate memory for *request_buffer\nError code: %d\n", errno);
+                free(*request_buffer);
+                *request_buffer = NULL;
+                return -1;
+            }
+        } else {
+            break;
+        }
+    }
+
+    if (bytes_read == -1) {
+        fprintf(stderr, "Failed extract headers from *request_buffer\nError code: %d\n", errno);
+        free(*request_buffer);
+        *request_buffer = NULL;
+        return -1;
+    }
+
+    (*request_buffer)[buffer_size] = '\0';
+
+    return 0;
+}
+
+int parse_http_request(HttpRequest *parsed_http_request, const char *http_request) {
+    // 1. Extract http request method
+    const char *method_end = strchr(http_request, ' ');
+    size_t method_length = method_end - http_request;
+    if (memcpy(parsed_http_request->method, http_request, method_length) == NULL) {
+        fprintf(stderr, "Failed to copy method from http request into structure\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    parsed_http_request->method[method_length] = '\0';
+
+    // 2. Extract http request url and 3. url query params
+    const char *url_start = method_end + 1;
+    const char *url_end;
+    if ((url_end = strchr(url_start, ' ')) == NULL) {
+        fprintf(stderr, "Failed to find char\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    const char *query_params_start = url_start;
+
+    int query_params_start_index = -1; // We start assuming there isn't any query params
+    // Iterate until whitespace is found
+    while (!isspace((unsigned char)*query_params_start)) {
+        // If a '?' char is found along the way, that's the start of the query params
+        if (*query_params_start == '?') {
+            query_params_start_index = query_params_start - http_request;
+            break;
+        }
+
+        // Keep moving forward
+        query_params_start++;
+    }
+
+    size_t query_params_length = 0;
+    if (query_params_start_index >= 0) {
+        query_params_length = url_end - (query_params_start + 1); // Skip '?' char found at the beginning of query params
+
+        // Because we separate the url and url query params, we set
+        // the end of the url to where the url query params start
+        url_end = &http_request[query_params_start_index];
+    }
+
+    size_t url_len = url_end - url_start;
+
+    if (memcpy(parsed_http_request->url, url_start, url_len) == NULL) {
+        fprintf(stderr, "Failed to copy url from http request into structure\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    parsed_http_request->url[url_len] = '\0';
+
+    if (query_params_length > 0) {
+        parsed_http_request->query_params = (char *)malloc(query_params_length * (sizeof parsed_http_request->query_params) + 1);
+        if (parsed_http_request->query_params == NULL) {
+            fprintf(stderr, "Failed to allocate memory for parsed_http_request->query_params\nError code: %d\n", errno);
+            http_request_free(parsed_http_request);
+            return -1;
+        }
+
+        // (query_params_start + 1) -> skip '?' char found at the beginning of query params
+        if (memcpy(parsed_http_request->query_params, (query_params_start + 1), query_params_length) == NULL) {
+            fprintf(stderr, "Failed to copy url query params from http request into structure\nError code: %d\n", errno);
+            http_request_free(parsed_http_request);
+            return -1;
+        }
+
+        parsed_http_request->query_params[query_params_length] = '\0';
+    }
+
+    // 4. Extract http version from request
+    char *http_version_start;
+    if ((http_version_start = strchr(query_params_start, ' ')) == NULL) {
+        http_request_free(parsed_http_request);
+        fprintf(stderr, "Failed to find char\nError code: %d\n", errno);
+        return -1;
+    }
+
+    http_version_start++; // Skip space
+
+    char *http_version_end;
+    if ((http_version_end = strstr(http_version_start, "\r\n")) == NULL) {
+        fprintf(stderr, "Failed to find string\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    size_t http_version_len = http_version_end - http_version_start;
+
+    if (memcpy(parsed_http_request->http_version, http_version_start, http_version_len) == NULL) {
+        fprintf(stderr, "Failed to copy http version from http request into structure\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    parsed_http_request->http_version[http_version_len] = '\0';
+
+    // 5. Extract http request headers
+    char *headers_start = http_version_end + 2; // Skip "\r\n"
+    char *headers_end;
+    if ((headers_end = strstr(headers_start, "\r\n\r\n")) == NULL) {
+        fprintf(stderr, "Failed to find string\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    size_t headers_len = headers_end - headers_start;
+    parsed_http_request->headers = (char *)malloc(headers_len * (sizeof parsed_http_request->headers) + 1);
+    if (parsed_http_request->headers == NULL) {
+        fprintf(stderr, "Failed to allocate memory for parsed_http_request->headers\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    if (memcpy(parsed_http_request->headers, headers_start, headers_len) == NULL) {
+        fprintf(stderr, "Failed to copy headers from http request into structure\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    parsed_http_request->headers[headers_len] = '\0';
+
+    // 6. Extract http request body
+    char *body_start = headers_end + 4; // Skip "\r\n\r\n"
+    char *body_end;
+    if ((body_end = strchr(body_start, '\0')) == NULL) {
+        fprintf(stderr, "Failed to find char\nError code: %d\n", errno);
+        http_request_free(parsed_http_request);
+        return -1;
+    }
+
+    size_t body_length = body_end - body_start;
+    if (body_length > 0) {
+        parsed_http_request->body = (char *)malloc(body_length * (sizeof parsed_http_request->body) + 1);
+        if (parsed_http_request->body == NULL) {
+            fprintf(stderr, "Failed to allocate memory for parsed_http_request->body\nError code: %d\n", errno);
+            http_request_free(parsed_http_request);
+            return -1;
+        }
+
+        if (memcpy(parsed_http_request->body, body_start, body_length) == NULL) {
+            fprintf(stderr, "Failed to copy request body from http request into structure\nError code: %d\n", errno);
+            http_request_free(parsed_http_request);
+            return -1;
+        }
+
+        parsed_http_request->body[body_length] = '\0';
+    }
+
+    return 0;
+}
+
+void http_request_free(HttpRequest *parsed_http_request) {
+    free(parsed_http_request->query_params);
+    parsed_http_request->query_params = NULL;
+
+    free(parsed_http_request->body);
+    parsed_http_request->body = NULL;
+
+    free(parsed_http_request->headers);
+    parsed_http_request->headers = NULL;
+}
+
+int home_get(int client_socket, HttpRequest *request, uint8_t thread_index) {
+    printf("%s\n", request->url);
+
     char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
                       "<html><body><h1>Hello world!</h1></body></html>";
 
@@ -253,6 +513,22 @@ int home_get(int client_socket, uint8_t thread_index) {
     }
 
     close(client_socket);
+    return 0;
+}
+
+int not_found(int client_socket, HttpRequest *request) {
+    printf("%s\n", request->url);
+
+    char response[] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+                      "<html><body><h1>404 Not Found</h1></body></html>";
+
+    if (send(client_socket, response, strlen(response), 0) == -1) {
+        fprintf(stderr, "Failed send HTTP response\nError code: %d\n", errno);
+        return -1;
+    }
+
+    close(client_socket);
+
     return 0;
 }
 
